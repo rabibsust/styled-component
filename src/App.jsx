987743abe4a34e2df039996e6bab7e95123e3228
Ahmad Jamaly Rabib <rabib@japantravel.com>
2023-06-12 00:13:40 +0600

    nest styles in style component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
 import H1 from "./components/Title"
 import { DefaultButton } from "./components/Buttons"
 import { ExtendedButton } from "./components/Buttons"
+import styled from "styled-components"
 
 function App() {
     return(
         <div>
-            <H1 color="blue">I am using styled component</H1>
-            <p>This is testing paragraph for checking styled component</p>
-            <DefaultButton>Click Me!</DefaultButton>
-            <DefaultButton background="red">Click Me!</DefaultButton>
-            <ExtendedButton background="blue">Test Extended Button</ExtendedButton>
+            <Wrapper>
+                <H1 color="blue">I am using styled component</H1>
+                <p>This is testing paragraph for checking styled component</p>
+                <DefaultButton>Click Me!</DefaultButton>
+            </Wrapper>
         </div>
     )
 }
+
+const Wrapper = styled.div`
+h1 {
+    text-align: center;
+    color: violet;
+}
+p {
+    font-size: 40px;
+}
+
+button {
+    background-color: pink;
+    padding: 4px 8px;
+    border: none;
+}
+`
 
 export default App
